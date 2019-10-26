f59f4cd7ccda83d4c5849c7dc326cfaa38e4acd3
Tom Kidd <tom@reifyhealth.com>
2019-10-26 10:23:48 -0400

    Checkpoint for no longer requiring manual aptible identifier mapping
    Because determine-aptible-identifiers can use the app to determine
    stack/env/app ids, I have integrated it into -main so that the edn
    files don't have to be used to do it.
    
    Before any security scans are done, the security-scan-tuples are
    checked against the aptible-identifiers to ensure that all of them can
    be determined. If not, the page will close and tell you which ones it
    can't resolve, otherwise it will do the scans, like before.

--- examples/aptible.cljs
+++ examples/aptible.cljs
@@ -3,7 +3,8 @@
 
   APTIBLE_EMAIL=fake@email.com APTIBLE_PASSWORD=p@ssW0rD APTIBLE_MULTI_FACTOR_TOKEN=123456 lumo --classpath src:examples -m examples.aptible
 "
-  (:require [clojure.string :as str]
+  (:require [cljs.pprint :refer [pprint]]
+            [clojure.string :as str]
             [poe.core :as poe]
             [poe.file-export :as file-export]
             [poe.html.scrape :as html-scrape]
@@ -58,6 +59,10 @@
   to the \"Security Scan\" tab to run the scan and capture results"
   (slurp-edn security-scan-tuples-path))
 
+(def landing-page-selector
+  "The selector to click to bring you back to the expected landing page"
+  ".enclave-nav")
+
 (defn produce-login-actions
   ([]
    (produce-login-actions {:email              env-email
@@ -69,9 +74,7 @@
           multi-factor-token env-multi-factor-token}}]
    (let [email-selector              "input[type='email']"
          password-selector           "//input[@name='password' and @type='password']"
-         multi-factor-token-selector "//input[@name='otp-token' and @type='text']"
-
-         enclave-selector ".enclave-nav"]
+         multi-factor-token-selector "//input[@name='otp-token' and @type='text']"]
      [[:wait-until-located email-selector]
       [:wait-until-visible email-selector]
       [:input-text email-selector email {:enter? false}]
@@ -84,18 +87,17 @@
       [:wait-until-visible multi-factor-token-selector {:by :xpath}]
       [:input-text multi-factor-token-selector multi-factor-token {:enter? true :by :xpath}]
 
-      [:wait-until-located enclave-selector]
-      [:click enclave-selector]])))
+      [:wait-until-located landing-page-selector]
+      [:click landing-page-selector]])))
 
 (defn produce-security-scan-actions
-  [stack environment app]
-  (let [stack-selector            (str "a[href='/stack/" (stack-name->id stack) "']")
-        environment-selector      (str "a[href='/accounts/" (environment-name->id environment "']"))
-        app-selector              (str "a[href='/apps/" (app-name->id app) "']")
-        security-scan-selector    (str "a[href='/apps/" (app-name->id app) "/security-scan']")
+  [aptible-identifiers stack environment app]
+  (let [stack-selector            (str "a[href='/stack/" (get-in aptible-identifiers [:stack-name->id stack]) "']")
+        environment-selector      (str "a[href='/accounts/" (get-in aptible-identifiers [:environment-name->id environment] "']"))
+        app-selector              (str "a[href='/apps/" (get-in aptible-identifiers [:environment-name->app-name->app-id environment app]) "']")
+        security-scan-selector    (str "a[href='/apps/" (get-in aptible-identifiers [:environment-name->app-name->app-id environment app]) "/security-scan']")
         table-selector            "//table"
-        table-or-success-selector "//table|//*[contains(@class,'alert-success')]"
-        enclave-selector          ".enclave-nav"]
+        table-or-success-selector "//table|//*[contains(@class,'alert-success')]"]
     [[:wait-until-located stack-selector]
      [:wait-until-visible stack-selector]
      [:click stack-selector]
@@ -129,27 +131,8 @@
              file      (str directory "/" filename)]
          (js/console.log (str "Exporting table data to: " file))
          (file-export/csv file rows)))
-     [:wait-until-located enclave-selector]
-     [:click enclave-selector]]))
-
-(defn validate-edn-configuration
-  "Makes sure that for every stack/env/app in security-scan-tuples, there are mappings"
-  []
-  (reduce (fn [acc [stack env app]]
-            (cond-> acc
-              (nil? (stack-name->id stack))
-              (update :errors conj {:type :stack-name-mapping
-                                    :msg  (str "Stack " stack " doesn't have an entry in the stack-name->id map.")})
-
-              (nil? (environment-name->id env))
-              (update :errors conj {:type :environment-name-mapping
-                                    :msg  (str "Environment " env " doesn't have an entry in the environment-name->id map.")})
-
-              (nil? (app-name->id app))
-              (update :errors conj {:type :app-name-mapping
-                                    :msg  (str "App " app " doesn't have an entry in the app-name->id map.")})))
-          {:errors []}
-          security-scan-tuples))
+     [:wait-until-located landing-page-selector]
+     [:click landing-page-selector]]))
 
 (defn determine-aptible-identifiers
   "Returns a promise that will eventually know all stack/env/app ids, and provide lookups to get from names to the ids.
@@ -162,34 +145,36 @@
         by             :xpath]
     (-> (.findElements poe/driver (poe/by* by stack-selector))
         (.then (fn [stack-web-elements]
-                 (let [name-href-tuple-promises (mapv
-                                                 (fn [e]
-                                                   (-> e
-                                                       (.getAttribute "href")
-                                                       (.then (fn [href]
-                                                                (.then (.getText e)
-                                                                       (fn [name]
-                                                                         [(str/lower-case name) href]))))))
-                                                 stack-web-elements)]
+                 (let [name-href-tuple-promises
+                       (mapv
+                        (fn [e]
+                          (-> e
+                              (.getAttribute "href")
+                              (.then (fn [href]
+                                       (.then (.getText e)
+                                              (fn [name]
+                                                [(str/lower-case name) href]))))))
+                        stack-web-elements)]
                    (js/Promise.all (clj->js name-href-tuple-promises)))))
         (.then (fn [name-href-tuples]
-                 (let [result (reduce (fn [acc [name href]]
-                                        (cond
-                                          ;; accounts correspond to environments
-                                          (str/includes? href "/accounts/")
-                                          (-> acc
-                                              (update :environment-name-href-tuples conj [name href])
-                                              (assoc-in [:environment-name->id name] (last (str/split href #"/"))))
+                 (let [result
+                       (reduce (fn [acc [name href]]
+                                 (cond
+                                   ;; accounts correspond to environments
+                                   (str/includes? href "/accounts/")
+                                   (-> acc
+                                       (update :environment-name-href-tuples conj [name href])
+                                       (assoc-in [:environment-name->id name] (last (str/split href #"/"))))
 
-                                          (str/includes? href "/stack/")
-                                          (assoc-in acc [:stack-name->id name] (last (str/split href #"/")))))
-                                      {:stack-name->id                     {}
-                                       :environment-name->id               {}
-                                       :environment-name-href-tuples       []
-                                       :environment-name->app-name->app-id {}}
-                                      ;; There is an item with a nil href for creating a new environment
-                                      (filter #(some? (second %))
-                                              name-href-tuples))]
+                                   (str/includes? href "/stack/")
+                                   (assoc-in acc [:stack-name->id name] (last (str/split href #"/")))))
+                               {:stack-name->id                     {}
+                                :environment-name->id               {}
+                                :environment-name-href-tuples       []
+                                :environment-name->app-name->app-id {}}
+                               ;; There is an item with a nil href for creating a new environment
+                               (filter #(some? (second %))
+                                       name-href-tuples))]
                    (js/Promise.resolve result))))
         (.then (fn [result]
                  (js/Promise.all (clj->js [(js/Promise.resolve result)
@@ -199,61 +184,99 @@
         (.then (fn [{:keys [environment-name-href-tuples hostname] :as result}]
                  ;; Navigate to each environment to scrape the apps that are there
                  ;; Note, reduce is used to make this happen sequentially, since js/Promise.all doesn't ensure that!
-                 (-> (reduce (fn [acc-promise [env-name href]]
-                               (let [relative-href (second (str/split href hostname))]
-                                 (.then acc-promise
-                                        (fn [acc]
-                                          (-> (.findElement poe/driver (poe/by* :xpath (str "//a[@href='" relative-href "']")))
-                                              (.click)
-                                              ;; Wait for page to load
-                                              (.then (fn [_]
-                                                       (.wait poe.core/driver
-                                                              (fn []
-                                                                (-> (.getCurrentUrl poe/driver)
-                                                                    (.then (fn [url] (str/includes? url href)))))
-                                                              5000)))
-                                              (.then (fn [_]
-                                                       (.findElements poe/driver (poe/by* :xpath "//*[contains(@class, 'resource-row app')]"))))
-                                              (.then (fn [app-elements]
-                                                       (let [name-id-tuple-promises (mapv
-                                                                                     (fn [e]
-                                                                                       (-> (.getAttribute e "href")
-                                                                                           (.then (fn [href]
-                                                                                                    (-> (.findElement e (poe/by* :css ".resource-row__title"))
-                                                                                                        (.then (fn [t]
-                                                                                                                 (.getText t)))
-                                                                                                        (.then (fn [name]
-                                                                                                                 ;; href in the form https://dashboard.aptible.com/apps/123456, we just want the id
-                                                                                                                 (js/Promise.resolve [name (last (str/split href #"/"))]))))))))
-                                                                                     app-elements)]
-                                                         (js/Promise.all (clj->js name-id-tuple-promises)))))
-                                              (.then (fn [name-id-tuples]
-                                                       (js/Promise.resolve (into {} (js->clj name-id-tuples)))))
-                                              (.then (fn [app-map]
-                                                       (js/Promise.resolve (assoc-in acc [:environment-name->app-name->app-id env-name] app-map)))))))))
+                 (-> (reduce
+                      (fn [acc-promise [env-name href]]
+                        (let [relative-href (second (str/split href hostname))]
+                          (.then acc-promise
+                                 (fn [acc]
+                                   (-> (.findElement poe/driver (poe/by* :xpath (str "//a[@href='" relative-href "']")))
+                                       (.click)
+                                       ;; Wait for page to load
+                                       (.then (fn [_]
+                                                (.wait poe.core/driver
+                                                       (fn []
+                                                         (-> (.getCurrentUrl poe/driver)
+                                                             (.then (fn [url] (str/includes? url href)))))
+                                                       5000)))
+                                       (.then (fn [_]
+                                                (.findElements poe/driver (poe/by* :xpath "//*[contains(@class, 'resource-row app')]"))))
+                                       (.then (fn [app-elements]
+                                                (let [name-id-tuple-promises
+                                                      (mapv
+                                                       (fn [e]
+                                                         (-> (.getAttribute e "href")
+                                                             (.then (fn [href]
+                                                                      (-> (.findElement e (poe/by* :css ".resource-row__title"))
+                                                                          (.then (fn [t]
+                                                                                   (.getText t)))
+                                                                          (.then (fn [name]
+                                                                                   ;; href in the form https://dashboard.aptible.com/apps/123456, we just want the id
+                                                                                   (js/Promise.resolve
+                                                                                    [name (last (str/split href #"/"))]))))))))
+                                                       app-elements)]
+                                                  (js/Promise.all (clj->js name-id-tuple-promises)))))
+                                       (.then (fn [name-id-tuples]
+                                                (js/Promise.resolve (into {} (js->clj name-id-tuples)))))
+                                       (.then (fn [app-map]
+                                                (js/Promise.resolve (assoc-in acc [:environment-name->app-name->app-id env-name] app-map)))))))))
                              (js/Promise.resolve result)
                              environment-name-href-tuples)
-                     (.then (fn [result]
-                              (println {:final-result result})
-                              result))))))))
+                     (.then (fn [result] result))))))))
+
+(defn validate-edn-configuration
+  "Makes sure that for every stack/env/app in security-scan-tuples, there are mappings"
+  [aptible-identifiers security-scan-tuples]
+  (reduce (fn [acc [stack env app]]
+            (cond-> acc
+              (nil? (get-in aptible-identifiers [:stack-name->id stack]))
+              (update :errors conj {:type :stack-name-mapping
+                                    :msg  (str "Stack '" stack "' doesn't have an entry in the stack-name->id map.")})
+
+              (nil? (get-in aptible-identifiers [:environment-name->id env]))
+              (update :errors conj {:type :environment-name-mapping
+                                    :msg  (str "Environment '" env "' doesn't have an entry in the environment-name->id map.")})
+
+              (nil? (get-in aptible-identifiers [:environment-name->app-name->app-id env app]))
+              (update :errors conj {:type :app-name-mapping
+                                    :msg  (str "App '" app "' doesn't have an entry in the environment-name->app-name->id map.")})))
+          {:errors []}
+          security-scan-tuples))
 
 (defn -main
   "Use config files to identify aptible apps, run the \"Security Scan\" on them,
   and capture the results. All done through selenium webdriver via the aptible web ui."
   [& _]
-  (let [url "https://dashboard.aptible.com/login"
-        {:keys [errors]} (validate-edn-configuration)]
-    (if (seq errors)
-      (do
-        (println "Errors detected:")
-        (doseq [error errors]
-          (println (:msg error))))
-      (poe/run
-        {:url   url
-         :quit? true}
-        (reduce (fn [acc cur] (into acc (apply produce-security-scan-actions cur)))
-                (produce-login-actions)
-                security-scan-tuples)))))
+  (let [url "https://dashboard.aptible.com/login"]
+    (.then (poe/run
+             {:url   url
+              :quit? false}
+             (into (produce-login-actions)
+                   [(fn [_]
+                      (-> (determine-aptible-identifiers)
+                          (.then (fn [aptible-identifiers]
+                                   (let [validation-result (validate-edn-configuration aptible-identifiers security-scan-tuples)]
+                                     {:aptible-identifiers aptible-identifiers
+                                      :validation-result   validation-result})))
+                          (.then (fn [{:keys [aptible-identifiers validation-result]}]
+                                   (let [{:keys [errors]} validation-result]
+                                     (if (seq errors)
+                                       (do
+                                         (println "Errors detected:")
+                                         (doseq [error errors]
+                                           (pprint error))
+                                         (-> (.close poe/driver)
+                                             (.then #(.quit poe/driver))
+                                             (.then #(.exit js/process 1))))
+                                       aptible-identifiers))))))]))
+           (fn [aptible-identifiers]
+             (poe/run
+               {:promise (js/Promise.resolve nil)
+                :quit?   true}
+               (reduce (fn [acc cur] (into acc (apply produce-security-scan-actions
+                                                      aptible-identifiers
+                                                      cur)))
+                       [[:click landing-page-selector]]
+                       security-scan-tuples))))))
 
 (comment
   (require 'poe.core
